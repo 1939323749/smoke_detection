cea4dca2d0aaea91cd2d84124e3dc6964efbecaf
1939323749 <1939323749@qq.com>
2023-11-03 22:25:31 +0800

    fix: a file not committed

--- app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -1,25 +1,7 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.cupcake
 
-import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -47,27 +29,15 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.cupcake.data.DataSource
-import com.example.cupcake.data.OrderUiState
 import com.example.cupcake.ui.*
 
-/**
- * enum values that represent the screens in the app
- */
 enum class CupcakeScreen(@StringRes val title: Int) {
     Start(title = R.string.app_name),
     SourceFile(title = R.string.source_file),
     SourceCamera(title = R.string.source_camera),
     Result(title = R.string.result),
-    Flavor(title = R.string.choose_flavor),
-    AdditionalItems(title = R.string.additional_items),
-    SelectItemQuantity(title = R.string.select_item_quantity),
-    Pickup(title = R.string.choose_pickup_date),
-    Summary(title = R.string.order_summary)
 }
 
-/**
- * Composable that displays the topBar and displays back button if back navigation is possible.
- */
 @Composable
 fun CupcakeAppBar(
     currentScreen: CupcakeScreen,
@@ -141,15 +111,12 @@ fun CupcakeApp(
                 )
             }
             composable(route = CupcakeScreen.SourceFile.name) {
-                val context = LocalContext.current
                 SourceFileScreen(
                     uri=uiState.uri,
-                    subtotal = uiState.price,
                     onNextButtonClicked = { navController.navigate(CupcakeScreen.Result.name) },
                     onCancelButtonClicked = {
                         cancelOrderAndNavigateToStart(viewModel, navController)
                     },
-                    options = DataSource.flavors.map { id -> context.resources.getString(id) },
                     onImageSelected = { viewModel.setImage(it) },
                     modifier = Modifier.fillMaxHeight()
                 )
@@ -158,12 +125,10 @@ fun CupcakeApp(
                 val context = LocalContext.current
                 SourceCameraScreen(
                     imageUri = uiState.uri,
-                    subtotal = uiState.price,
                     onNextButtonClicked = { navController.navigate(CupcakeScreen.Result.name) },
                     onCancelButtonClicked = {
                         cancelOrderAndNavigateToStart(viewModel, navController)
                     },
-                    options = DataSource.flavors.map { id -> context.resources.getString(id) },
                     onPhotoShot = { viewModel.setImage(it) },
                     modifier = Modifier.fillMaxHeight()
                 )
@@ -179,108 +144,14 @@ fun CupcakeApp(
                     modifier = Modifier.fillMaxHeight()
                 )
             }
-            composable(route = CupcakeScreen.Flavor.name) {
-                val context = LocalContext.current
-                SelectOptionScreen(
-                    image = null,
-                    subtotal = uiState.price,
-                    onNextButtonClicked = { navController.navigate(CupcakeScreen.AdditionalItems.name) },
-                    onCancelButtonClicked = {
-                        cancelOrderAndNavigateToStart(viewModel, navController)
-                    },
-                    options = DataSource.flavors.map { id -> context.resources.getString(id) },
-                    onSelectionChanged = { viewModel.setFlavor(it) },
-                    modifier = Modifier.fillMaxHeight()
-                )
-            }
-            composable(route = CupcakeScreen.AdditionalItems.name) {
-                val context = LocalContext.current
-                var next by remember { mutableStateOf(CupcakeScreen.Pickup.name) }
-                SelectOptionScreen(
-                    image = null,
-                    subtotal = uiState.price,
-                    onNextButtonClicked = { navController.navigate(next )},
-                    onCancelButtonClicked = {
-                        cancelOrderAndNavigateToStart(viewModel, navController)
-                    },
-                    options = DataSource.additionalItems.map { id -> context.resources.getString(id) },
-                    onSelectionChanged = {
-                        next=if(it!="None"){CupcakeScreen.SelectItemQuantity.name}else{CupcakeScreen.Pickup.name}
-                        Log.d("test",next)
-                        viewModel.setAdditionalItems(it)
-                    },
-                    modifier = Modifier.fillMaxHeight()
-                )
-            }
-            composable(route = CupcakeScreen.SelectItemQuantity.name) {
-                SelectOptionScreen(
-                    image = null,
-                    subtotal = uiState.price,
-                    onNextButtonClicked = { navController.navigate(CupcakeScreen.Pickup.name) },
-                    onCancelButtonClicked = {
-                        cancelOrderAndNavigateToStart(viewModel, navController)
-                    },
-                    options = DataSource.itemQuantity ,
-                    onSelectionChanged = {
-                        viewModel.setItemQuantity(it) },
-                    modifier = Modifier.fillMaxHeight()
-                )
-            }
-            composable(route = CupcakeScreen.Pickup.name) {
-                SelectOptionScreen(
-                    image = null,
-                    subtotal = uiState.price,
-                    onNextButtonClicked = { navController.navigate(CupcakeScreen.Summary.name) },
-                    onCancelButtonClicked = {
-                        cancelOrderAndNavigateToStart(viewModel, navController)
-                    },
-                    options = uiState.pickupOptions,
-                    onSelectionChanged = { viewModel.setDate(it) },
-                    modifier = Modifier.fillMaxHeight()
-                )
-            }
-            composable(route = CupcakeScreen.Summary.name) {
-                val context = LocalContext.current
-                OrderSummaryScreen(
-                    orderUiState = uiState,
-                    onCancelButtonClicked = {
-                        cancelOrderAndNavigateToStart(viewModel, navController)
-                    },
-                    onSendButtonClicked = { subject: String, summary: String ->
-                        shareOrder(context, subject = subject, summary = summary)
-                    },
-                    modifier = Modifier.fillMaxHeight()
-                )
-            }
         }
     }
 }
 
-/**
- * Resets the [OrderUiState] and pops up to [CupcakeScreen.Start]
- */
 private fun cancelOrderAndNavigateToStart(
     viewModel: OrderViewModel,
     navController: NavHostController
 ) {
     viewModel.resetOrder()
     navController.popBackStack(CupcakeScreen.Start.name, inclusive = false)
-}
-
-/**
- * Creates an intent to share order details
- */
-private fun shareOrder(context: Context, subject: String, summary: String) {
-    // Create an ACTION_SEND implicit intent with order details in the intent extras
-    val intent = Intent(Intent.ACTION_SEND).apply {
-        type = "text/plain"
-        putExtra(Intent.EXTRA_SUBJECT, subject)
-        putExtra(Intent.EXTRA_TEXT, summary)
-    }
-    context.startActivity(
-        Intent.createChooser(
-            intent,
-            context.getString(R.string.new_cupcake_order)
-        )
-    )
 }
